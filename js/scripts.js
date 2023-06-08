13587c1fc733968d482c1ee5d8e994a3103e5617
Jenny Parsons <jennylynparsons@gmail.com>
2023-06-07 21:59:54 -0600

    add subtract, add, and divide functionality

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,51 @@ function add(number1,number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert((number1) + "+" + (number2) + "=" + add(number1, number2));
+
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
+// business logic
+function subtract(number3,number4) {
+  return number3 - number4;
+}
+
+// user interface logic 
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+
+window.alert((number3) + "-" + (number4) + "=" + subtract(number3, number4));
+
+
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
+// business logic
+function multiply(number5,number6) {
+  return number5 * number6;
+}
+
+// user interface logic 
+const number5 = parseInt(prompt("Enter a number:"));
+const number6 = parseInt(prompt("Enter another number:"));
+
+window.alert("The multiplication of your function equals " + multiply(number5,number6));
+
+function saySomething(whatToSay) {
+  window.alert(whatToSay)
+}
+
+// business logic
+function divide(number7,number8) {
+  return number7 / number8;
+}
+
+  // user interface logic 
+const number7 = parseInt(prompt("Enter a number:"));
+const number8 = parseInt(prompt("Enter another number:"));
+
+window.alert("The division of your function equals " +divide(number7,number8));
 
